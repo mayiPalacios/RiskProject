6bf2b717ad72d0f1ebd0e4d0972df50f3ff55cd3
mayiPalacios <00018619@uca.edu.sv>
2023-06-11 23:37:47 -0600

    add convert methods and download btn for file

--- src/components/form.js
+++ src/components/form.js
@@ -1,55 +1,115 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Form = () => {
   const [typeD, setTipeD] = useState("");
   const [dataText, setDataText] = useState("");
-  const [txtD, setTxt] = useState(null);
+  const [delimiter, setDelimiter] = useState("");
+  const [isFieldEmpty, setIsFieldEmpty] = useState(false);
+  const [fileDownload, setFileDownload] = useState(null);
+  const [key, setKey] = useState("");
+  const [caseD, setcaseD] = useState("");
+  const [docu, setdocu] = useState(null);
   const handleDocumentType = (event) => {
-    setTipeD(event.target.value);
+    if (event.target.value !== "txt to xml") {
+      setTipeD(event.target.value);
+      setcaseD(event.target.value);
+    } else {
+      setTipeD(".txt");
+      setcaseD("txt to xml");
+    }
   };
 
   const handleFileChange = (event) => {
-    switch (typeD) {
-      case ".txt":
-        const file = event.target.files[0];
-        setTxt(file);
-
-        break;
-
-      case ".XML":
-        break;
-
-      case ".json":
-        break;
-
-      default:
-        break;
-    }
+    const file = event.target.files[0];
+    setdocu(file);
   };
-  /*
-  useEffect(() => {
-   
-    fetchCharacter();
-  }, [txtD]);*/
+
+  const handleDelimiter = (event) => {
+    setDelimiter(event.target.value);
+  };
+
+  const handleKey = (event) => {
+    setKey(event.target.value);
+  };
 
   const fetchDocument = async (event) => {
     event.preventDefault();
     try {
       const formData = new FormData();
+      let response;
+      if (key === "" || delimiter === "") {
+        setIsFieldEmpty(true);
+        return;
+      }
+      setIsFieldEmpty(false);
+      switch (caseD) {
+        case "txt to xml":
+          formData.append("file", docu);
 
-      formData.append("file", txtD);
+          response = await axios.post(
+            `http://localhost:3000/convert_txt_to_xml/${delimiter}/${key}`,
+            formData
+          );
 
-      const response = await axios.post(
-        "http://localhost:3000/convert_txt_to_xml",
-        formData
-      );
+          setDataText(response.data);
+          setFileDownload(
+            new Blob([response.data], { type: "application/xml" })
+          );
+          break;
 
-      setDataText(response.data);
+        case ".txt":
+          formData.append("file", docu);
+
+          response = await axios.post(
+            `http://localhost:3000/convert_txt_to_json/;/${key}`,
+            formData
+          );
+
+          setDataText(JSON.stringify(response.data));
+          setFileDownload(
+            new Blob([JSON.stringify(response.data)], {
+              type: "application/json",
+            })
+          );
+
+          break;
+        case ".XML":
+          formData.append("file", docu);
+          response = await axios.post(
+            `http://localhost:3000/convert_xml_to_txt/${delimiter}/${key}`,
+            formData
+          );
+          setDataText(response.data);
+          setFileDownload(new Blob([response.data], { type: "text/plain" }));
+          break;
+
+        case ".json":
+          formData.append("file", docu);
+          response = await axios.post(
+            `http://localhost:3000/convert_json_to_txt/${delimiter}/${key}`,
+            formData
+          );
+          setDataText(response.data);
+          setFileDownload(new Blob([response.data], { type: "text/plain" }));
+          break;
+        default:
+          break;
+      }
     } catch (error) {
       console.error("error :(", error);
       console.log("Error response:", error.response);
     }
+  };
+
+  const handleDownload = () => {
+    console.log(fileDownload);
+    const url = URL.createObjectURL(fileDownload);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "file to download";
+    link.click();
+    URL.revokeObjectURL(url);
   };
 
   return (
@@ -64,12 +124,17 @@ const Form = () => {
                 id="input-name"
                 className="form-input"
                 placeholder=" "
+                onChange={handleDelimiter}
               />
               <label for="" className="form-label">
                 Delimiter symbol
               </label>
               <span className="form-line"></span>
-              <p className="alert-input alert-input-name">
+              <p
+                className={`alert-input-last ${
+                  isFieldEmpty ? "" : "alert-input"
+                }  `}
+              >
                 This section is mandatory
               </p>
             </div>
@@ -81,16 +146,22 @@ const Form = () => {
                 name="last-name"
                 id="last-name"
                 placeholder=" "
+                onChange={handleKey}
               />
               <label for="" className="form-label">
                 Encryption key
               </label>
               <span className="form-line"></span>
-              <p className="alert-input alert-input-last">
+              <p
+                className={`alert-input-last ${
+                  isFieldEmpty ? "" : "alert-input"
+                }  `}
+              >
                 This section is mandatory
               </p>
             </div>
           </div>
+
           <div className="sidebar-box">
             <select
               className="styled-select"
@@ -99,14 +170,20 @@ const Form = () => {
             >
               <option value="">document type</option>
               <option value=".txt">text to JSON</option>
-              <option value=".txt">text to XML</option>
-              <option value=".XML">XML to JSON</option>
-              <option value=".json">JSON to XML</option>
+              <option value="txt to xml">text to XML</option>
+              <option value=".XML">XML to txt</option>
+              <option value=".json">JSON to txt</option>
             </select>
           </div>
 
-          <div className="form-group">
-            <input type="file" accept={typeD} onChange={handleFileChange} />
+          <div className="flex-fileDocument">
+            <div className="form-group">
+              <input type="file" accept={typeD} onChange={handleFileChange} />
+            </div>
+
+            <div className="form-group">
+              <button onClick={handleDownload}>Download</button>
+            </div>
           </div>
 
           <button onClick={fetchDocument} id="save-reg">
@@ -114,8 +191,8 @@ const Form = () => {
           </button>
         </form>
       </div>
-      <div className="show__convert--doc">
-        <pre>{dataText}</pre>
+      <div className="show__convert--doc-container">
+        <pre className="show__convert--doc">{dataText}</pre>
       </div>
     </div>
   );
